e1ff52c90d4c595c15713c75a66e877f257fefb1
Sheraz Ahmad Khilji <sheraz.khilji@rocket-internet.de>
2016-07-27 17:48:28 +0200

    Imported to Android Studio

---
+++ videffects/src/main/java/com/sherazkhilji/videffects/interfaces/ShaderInterface.java
@@ -0,0 +1,23 @@
+package com.sherazkhilji.videffects.interfaces;
+
+import android.opengl.GLSurfaceView;
+
+/**
+ * An interface that every effect must implement so that there is a common
+ * getShader method that every effect class is force to override
+ * 
+ * @author sheraz.khilji
+ *
+ */
+public interface ShaderInterface {
+	/**
+	 * Returns Shader code
+	 * 
+	 * @param mGlSurfaceView
+	 *            send this for every shader but this will only be used when the
+	 *            shader needs it.
+	 * @return complete shader code in C
+	 */
+	public String getShader(GLSurfaceView mGlSurfaceView);
+
+}
---
+++ videffects/src/main/java/com/sherazkhilji/videffects/view/VideoSurfaceView.java
@@ -0,0 +1,348 @@
+package com.sherazkhilji.videffects.view;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.SurfaceTexture;
+import android.media.MediaPlayer;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.Surface;
+import android.widget.Toast;
+
+import com.sherazkhilji.videffects.NoEffect;
+import com.sherazkhilji.videffects.interfaces.ShaderInterface;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * This GLSurfaceView can be used to display video that is being played by media
+ * player and at the same time different effect can be applied on the video.
+ * This view uses shader for applying different effects.
+ *
+ * @author sheraz.khilji
+ */
+@SuppressLint("ViewConstructor")
+public class VideoSurfaceView extends GLSurfaceView {
+    private static final String TAG = "VideoSurfaceView";
+    private VideoRender mRenderer;
+    private MediaPlayer mMediaPlayer = null;
+    private static VideoSurfaceView mSurfaceView;
+    private Context mContext;
+    private static ShaderInterface effect;
+
+    public VideoSurfaceView(Context context) {
+        super(context);
+        mContext = context;
+        setEGLContextClientVersion(2);
+        mRenderer = new VideoRender(mContext);
+        setRenderer(mRenderer);
+        mSurfaceView = this;
+    }
+
+    public VideoSurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mContext = context;
+        setEGLContextClientVersion(2);
+        mRenderer = new VideoRender(mContext);
+        setRenderer(mRenderer);
+        mSurfaceView = this;
+    }
+
+    /**
+     * initializes media player and the effect that is going to be applied on
+     * video. The video is played automatically so you dont need to call play.
+     *
+     * @param mediaPlayer
+     */
+    public void init(MediaPlayer mediaPlayer, ShaderInterface shaderEffect) {
+        if (mediaPlayer == null)
+            Toast.makeText(mContext, "Set MediaPlayer before continuing",
+                    Toast.LENGTH_LONG).show();
+        else
+            mMediaPlayer = mediaPlayer;
+        if (shaderEffect == null)
+            effect = new NoEffect();
+        else
+            effect = shaderEffect;
+    }
+
+    @Override
+    public void onResume() {
+        if (mMediaPlayer == null) {
+            Log.e(TAG, "Call init() before Continuing");
+            return;
+        }
+        queueEvent(new Runnable() {
+            @Override
+            public void run() {
+                mRenderer.setMediaPlayer(mMediaPlayer);
+            }
+        });
+
+        super.onResume();
+    }
+
+    private static class VideoRender implements Renderer,
+            SurfaceTexture.OnFrameAvailableListener {
+        private static String TAG = "VideoRender";
+
+        private static final int FLOAT_SIZE_BYTES = 4;
+        private static final int TRIANGLE_VERTICES_DATA_STRIDE_BYTES = 5 * FLOAT_SIZE_BYTES;
+        private static final int TRIANGLE_VERTICES_DATA_POS_OFFSET = 0;
+        private static final int TRIANGLE_VERTICES_DATA_UV_OFFSET = 3;
+        private final float[] mTriangleVerticesData = {
+                // X, Y, Z, U, V
+                -1.0f, -1.0f, 0, 0.f, 0.f, 1.0f, -1.0f, 0, 1.f, 0.f, -1.0f,
+                1.0f, 0, 0.f, 1.f, 1.0f, 1.0f, 0, 1.f, 1.f,};
+
+        private FloatBuffer mTriangleVertices;
+
+        private final String mVertexShader = "uniform mat4 uMVPMatrix;\n"
+                + "uniform mat4 uSTMatrix;\n" + "attribute vec4 aPosition;\n"
+                + "attribute vec4 aTextureCoord;\n"
+                + "varying vec2 vTextureCoord;\n" + "void main() {\n"
+                + "  gl_Position = uMVPMatrix * aPosition;\n"
+                + "  vTextureCoord = (uSTMatrix * aTextureCoord).xy;\n" + "}\n";
+        private float[] mMVPMatrix = new float[16];
+        private float[] mSTMatrix = new float[16];
+
+        private int mProgram;
+        private int mTextureID[] = new int[2];
+        private int muMVPMatrixHandle;
+        private int muSTMatrixHandle;
+        private int maPositionHandle;
+        private int maTextureHandle;
+
+        private SurfaceTexture mSurface;
+        private boolean updateSurface = false;
+
+        private static int GL_TEXTURE_EXTERNAL_OES = 0x8D65;
+
+        private MediaPlayer mMediaPlayer;
+
+        public VideoRender(Context context) {
+            mTriangleVertices = ByteBuffer
+                    .allocateDirect(
+                            mTriangleVerticesData.length * FLOAT_SIZE_BYTES)
+                    .order(ByteOrder.nativeOrder()).asFloatBuffer();
+            mTriangleVertices.put(mTriangleVerticesData).position(0);
+
+            Matrix.setIdentityM(mSTMatrix, 0);
+        }
+
+        public void setMediaPlayer(MediaPlayer player) {
+            mMediaPlayer = player;
+        }
+
+        @Override
+        public void onDrawFrame(GL10 glUnused) {
+            synchronized (this) {
+                if (updateSurface) {
+                    mSurface.updateTexImage();
+                    mSurface.getTransformMatrix(mSTMatrix);
+                    updateSurface = false;
+                }
+            }
+            mProgram = createProgram(mVertexShader,
+                    effect.getShader(mSurfaceView));
+            GLES20.glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
+            GLES20.glClear(GLES20.GL_DEPTH_BUFFER_BIT
+                    | GLES20.GL_COLOR_BUFFER_BIT);
+
+            GLES20.glUseProgram(mProgram);
+            checkGlError("glUseProgram");
+
+            GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
+            GLES20.glBindTexture(GL_TEXTURE_EXTERNAL_OES, mTextureID[0]);
+
+            mTriangleVertices.position(TRIANGLE_VERTICES_DATA_POS_OFFSET);
+            GLES20.glVertexAttribPointer(maPositionHandle, 3, GLES20.GL_FLOAT,
+                    false, TRIANGLE_VERTICES_DATA_STRIDE_BYTES,
+                    mTriangleVertices);
+            checkGlError("glVertexAttribPointer maPosition");
+            GLES20.glEnableVertexAttribArray(maPositionHandle);
+            checkGlError("glEnableVertexAttribArray maPositionHandle");
+
+            mTriangleVertices.position(TRIANGLE_VERTICES_DATA_UV_OFFSET);
+            GLES20.glVertexAttribPointer(maTextureHandle, 3, GLES20.GL_FLOAT,
+                    false, TRIANGLE_VERTICES_DATA_STRIDE_BYTES,
+                    mTriangleVertices);
+            checkGlError("glVertexAttribPointer maTextureHandle");
+            GLES20.glEnableVertexAttribArray(maTextureHandle);
+            checkGlError("glEnableVertexAttribArray maTextureHandle");
+
+            Matrix.setIdentityM(mMVPMatrix, 0);
+            GLES20.glUniformMatrix4fv(muMVPMatrixHandle, 1, false, mMVPMatrix,
+                    0);
+            GLES20.glUniformMatrix4fv(muSTMatrixHandle, 1, false, mSTMatrix, 0);
+
+            GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4);
+            checkGlError("glDrawArrays");
+            GLES20.glFinish();
+
+        }
+
+        @Override
+        public void onSurfaceChanged(GL10 glUnused, int width, int height) {
+
+        }
+
+        @Override
+        public void onSurfaceCreated(GL10 glUnused, EGLConfig config) {
+
+            mProgram = createProgram(mVertexShader,
+                    effect.getShader(mSurfaceView));
+            if (mProgram == 0) {
+                return;
+            }
+            maPositionHandle = GLES20
+                    .glGetAttribLocation(mProgram, "aPosition");
+            checkGlError("glGetAttribLocation aPosition");
+            if (maPositionHandle == -1) {
+                throw new RuntimeException(
+                        "Could not get attrib location for aPosition");
+            }
+            maTextureHandle = GLES20.glGetAttribLocation(mProgram,
+                    "aTextureCoord");
+            checkGlError("glGetAttribLocation aTextureCoord");
+            if (maTextureHandle == -1) {
+                throw new RuntimeException(
+                        "Could not get attrib location for aTextureCoord");
+            }
+
+            muMVPMatrixHandle = GLES20.glGetUniformLocation(mProgram,
+                    "uMVPMatrix");
+            checkGlError("glGetUniformLocation uMVPMatrix");
+            if (muMVPMatrixHandle == -1) {
+                throw new RuntimeException(
+                        "Could not get attrib location for uMVPMatrix");
+            }
+
+            muSTMatrixHandle = GLES20.glGetUniformLocation(mProgram,
+                    "uSTMatrix");
+            checkGlError("glGetUniformLocation uSTMatrix");
+            if (muSTMatrixHandle == -1) {
+                throw new RuntimeException(
+                        "Could not get attrib location for uSTMatrix");
+            }
+
+            // int[] textures = new int[1];
+            GLES20.glGenTextures(2, mTextureID, 0);
+            // GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, mTextureID[0]);
+
+            // mTextureID = textures[0];
+            GLES20.glBindTexture(GL_TEXTURE_EXTERNAL_OES, mTextureID[0]);
+            checkGlError("glBindTexture mTextureID");
+
+            // GLES20.glTexParameterf(GL_TEXTURE_EXTERNAL_OES,
+            // GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_NEAREST);
+            // GLES20.glTexParameterf(GL_TEXTURE_EXTERNAL_OES,
+            // GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
+            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D,
+                    GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
+            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D,
+                    GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
+            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D,
+                    GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE);
+            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D,
+                    GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE);
+
+			/*
+             * Create the SurfaceTexture that will feed this textureID, and pass
+			 * it to the MediaPlayer
+			 */
+            mSurface = new SurfaceTexture(mTextureID[0]);
+            mSurface.setOnFrameAvailableListener(this);
+
+            Surface surface = new Surface(mSurface);
+            mMediaPlayer.setSurface(surface);
+            mMediaPlayer.setScreenOnWhilePlaying(true);
+            surface.release();
+
+            try {
+                mMediaPlayer.prepare();
+            } catch (IOException t) {
+                Log.e(TAG, "media player prepare failed");
+            }
+
+            synchronized (this) {
+                updateSurface = false;
+            }
+
+            mMediaPlayer.start();
+        }
+
+        @Override
+        synchronized public void onFrameAvailable(SurfaceTexture surface) {
+            updateSurface = true;
+        }
+
+        private int loadShader(int shaderType, String source) {
+            int shader = GLES20.glCreateShader(shaderType);
+            if (shader != 0) {
+                GLES20.glShaderSource(shader, source);
+                GLES20.glCompileShader(shader);
+                int[] compiled = new int[1];
+                GLES20.glGetShaderiv(shader, GLES20.GL_COMPILE_STATUS,
+                        compiled, 0);
+                if (compiled[0] == 0) {
+                    Log.e(TAG, "Could not compile shader " + shaderType + ":");
+                    Log.e(TAG, GLES20.glGetShaderInfoLog(shader));
+                    GLES20.glDeleteShader(shader);
+                    shader = 0;
+                }
+            }
+            return shader;
+        }
+
+        private int createProgram(String vertexSource, String fragmentSource) {
+            int vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, vertexSource);
+            if (vertexShader == 0) {
+                return 0;
+            }
+            int pixelShader = loadShader(GLES20.GL_FRAGMENT_SHADER,
+                    fragmentSource);
+            if (pixelShader == 0) {
+                return 0;
+            }
+
+            int program = GLES20.glCreateProgram();
+            if (program != 0) {
+                GLES20.glAttachShader(program, vertexShader);
+                checkGlError("glAttachShader");
+                GLES20.glAttachShader(program, pixelShader);
+                checkGlError("glAttachShader");
+                GLES20.glLinkProgram(program);
+                int[] linkStatus = new int[1];
+                GLES20.glGetProgramiv(program, GLES20.GL_LINK_STATUS,
+                        linkStatus, 0);
+                if (linkStatus[0] != GLES20.GL_TRUE) {
+                    Log.e(TAG, "Could not link program: ");
+                    Log.e(TAG, GLES20.glGetProgramInfoLog(program));
+                    GLES20.glDeleteProgram(program);
+                    program = 0;
+                }
+            }
+            return program;
+        }
+
+        private void checkGlError(String op) {
+            int error;
+            while ((error = GLES20.glGetError()) != GLES20.GL_NO_ERROR) {
+                Log.e(TAG, op + ": glError " + error);
+                throw new RuntimeException(op + ": glError " + error);
+            }
+        }
+
+    } // End of class VideoRender.
+
+} // End of class VideoSurfaceView.
